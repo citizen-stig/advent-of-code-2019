3022e5974d66f3eb18932100070ddd006af13711
Nikolay Golub <nikolay.v.golub@gmail.com>
2019-12-08 22:55:48 +0100

    Day 2 working

--- src/day4.rs
+++ src/day4.rs
@@ -1,14 +1,17 @@
 fn check_line(line: &[u8]) -> bool {
-    let mut has_two_same_adjacent = false;
+    let mut matching_digits = vec![0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
     let never_descending = line
         .windows(2)
         .filter(|window| {
-            has_two_same_adjacent = has_two_same_adjacent || window[0] == window[1];
+            if window[0] == window[1] {
+                matching_digits[window[1] as usize] += 1;
+            }
             window[0] > window[1]
         })
-        .count()
-        == 0;
-    has_two_same_adjacent && never_descending
+        .count() == 0;
+    let has_proper_matching = matching_digits.iter().filter(|c| **c == 1).count() > 0;
+    has_proper_matching && never_descending
 }
 
 fn number_into_vec(number: i32) -> Vec<u8> {
@@ -19,13 +22,9 @@ fn number_into_vec(number: i32) -> Vec<u8> {
         .collect::<Vec<u8>>()
 }
 
-fn check_number(number: i32) -> bool {
-    check_line(&number_into_vec(number))
-}
-
 pub fn solve() -> u32 {
     (136_818..685_979)
-        .filter(|number| check_number(*number))
+        .filter(|number| check_line(&number_into_vec(*number)))
         .count() as u32
 }
 
@@ -38,21 +37,20 @@ mod tests {
 
         #[test]
         fn test_positive() {
-            assert!(check_line(&vec![1, 1, 1, 1, 1, 1]));
+            assert!(check_line(&vec![1, 1, 2, 2, 3, 3]));
+            assert!(check_line(&vec![1, 1, 1, 1, 2, 2]));
             assert!(check_line(&vec![1, 1, 2, 3, 4, 5]));
             assert!(check_line(&vec![1, 2, 3, 3, 4, 5]));
             assert!(check_line(&vec![1, 2, 3, 4, 5, 5]));
             assert!(check_line(&vec![1, 2, 3, 4, 5, 5]));
-            assert!(check_line(&vec![1, 1, 1, 2, 2, 2]));
-            assert!(check_line(&vec![1, 1, 1, 1, 2, 3]));
         }
 
         #[test]
         fn test_negative() {
-            assert!(!check_line(&vec![2, 2, 3, 4, 5, 6]));
-            assert!(!check_line(&vec![1, 2, 3, 7, 8, 9]));
-            assert!(!check_line(&vec![2, 2, 3, 4, 5, 6]));
-            assert!(!check_line(&vec![1, 3, 5, 6, 7, 9]));
+            assert!(!check_line(&vec![1, 1, 1, 1, 1, 1]));
+            assert!(!check_line(&vec![1, 2, 3, 4, 4, 4]));
+            assert!(!check_line(&vec![1, 1, 1, 2, 2, 2]));
+            assert!(!check_line(&vec![1, 1, 1, 1, 2, 3]));
         }
     }
 
@@ -64,18 +62,5 @@ mod tests {
         assert_eq!(number_into_vec(136_818), vec![1, 3, 6, 8, 1, 8]);
         assert_eq!(number_into_vec(685_979), vec![6, 8, 5, 9, 7, 9]);
         assert_eq!(number_into_vec(111_123), vec![1, 1, 1, 1, 2, 3]);
-    }
-
-    mod test_check_number {
-        use super::*;
-
-        #[test]
-        fn test_positive() {
-            assert!(check_number(111_111));
-            assert!(check_number(111_123));
-            assert!(check_number(122_345));
-            assert!(check_number(112_345));
-            assert!(check_number(111_222));
-        }
     }
 }
