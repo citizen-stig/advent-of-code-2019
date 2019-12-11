e8d54c7d5cb312f1dfcb1cce04dd17d17a9c0cf1
Nikolay Golub <nikolay.v.golub@gmail.com>
2019-12-12 00:14:38 +0100

    Part 1 working

--- src/day6.rs
+++ src/day6.rs
@@ -1,23 +1,8 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
-use std::collections::{HashMap, HashSet};
+use std::collections::{HashMap};
 use std::collections::hash_map::Entry::{Occupied, Vacant};
-
-struct Object {
-    name: String,
-    on_orbit: Vec<Object>,
-}
-
-impl Object {
-    pub fn new(name: &str) -> Self {
-        Object { name: name.to_owned(), on_orbit: Vec::new() }
-    }
-
-    pub fn add_to_orbit(&mut self, object: Object) {
-        self.on_orbit.push(object);
-    }
-}
 
 
 fn count_orbits(lines: &Vec<String>) -> usize {
@@ -27,30 +12,36 @@ fn count_orbits(lines: &Vec<String>) -> usize {
         let children = match map.entry(names[0].to_owned()) {
             Vacant(entry) => {
                 entry.insert(Vec::<String>::new())
-            },
+            }
             Occupied(entry) => {
                 entry.into_mut()
             }
         };
         children.push(names[1].to_owned())
     }
-    println!("MAP: {:?}", map);
-    println!("Iterating...");
-    let mut queue: Vec<String> = vec!["COM".to_owned()];
+    let mut queue: Vec<Option<String>> = vec![Some("COM".to_owned()), None];
+
     let mut count = 0;
-    let mut extra = 0;
-    while !queue.is_empty() {
+    let mut distance_to_root = 1;
+    while queue.len() > 1 {
         let current = queue.remove(0);
-        match map.get_mut(&current) {
-            Some(children) => {
-                count += children.len() + extra;
-                queue.append(children);
+        match current {
+            Some(current_name) => {
+                match map.get_mut(&current_name) {
+                    Some(children) => {
+                        count += children.len() * distance_to_root;
+                        children.iter().for_each(|s| queue.push(Some(s.clone())));
+                    }
+                    None => (),
+                }
             },
-            None => (),
+            None => {
+                distance_to_root += 1;
+                queue.push(None)
+            }
         }
-        extra += 1
+
     }
-    println!("Count: {:?}", count);
     count
 }
 
@@ -59,14 +50,13 @@ pub fn solve() -> usize {
     let file = File::open(filename).unwrap();
     let reader = BufReader::new(file);
 
-    let mut lines: Vec<String> = reader
+    let lines: Vec<String> = reader
         .lines()
         .map(|l| l.expect("Could not parse line"))
         .collect();
 
 
     count_orbits(&lines)
-//    3
 }
 
 #[cfg(test)]
@@ -75,27 +65,48 @@ mod tests {
 
     #[test]
     fn test_one() {
-        let lines = vec!["COM)B"];
+        let lines = vec!["COM)B".to_owned()];
 
         assert_eq!(count_orbits(&lines), 1);
     }
 
     #[test]
     fn test_two() {
-        let lines = vec!["COM)B", "COM)C"];
+        let lines = vec!["COM)B", "COM)C"].iter().map(|s| String::from(*s)).collect();
 
         assert_eq!(count_orbits(&lines), 2);
     }
 
     #[test]
     fn test_tree() {
-        let lines = vec!["COM)B", "B)C"].iter().map(|s| s.to_owned()).collect();
+        let lines = vec!["COM)B", "B)C"].iter().map(|s| String::from(*s)).collect();
 
         assert_eq!(count_orbits(&lines), 3);
     }
 
     #[test]
-    #[ignore]
+    fn test_four() {
+        let lines = vec![
+            "COM)B",
+            "B)C",
+            "COM)D"
+        ].iter().map(|s| String::from(*s)).collect();
+
+        assert_eq!(count_orbits(&lines), 4);
+    }
+
+    #[test]
+    fn test_seven() {
+        let lines = vec![
+            "COM)B",
+            "B)C",
+            "B)D"
+        ].iter().map(|s| String::from(*s)).collect();
+
+        assert_eq!(count_orbits(&lines), 5);
+    }
+
+    #[test]
     fn test_example() {
         let lines = vec!["COM)B",
                          "B)C",
@@ -107,7 +118,7 @@ mod tests {
                          "D)I",
                          "E)J",
                          "J)K",
-                         "K)L", ].iter().map(|s| s.to_owned()).collect();
+                         "K)L", ].iter().map(|s| String::from(*s)).collect();
 
         assert_eq!(count_orbits(&lines), 42);
     }
